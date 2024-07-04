afe247bda18060d6323fd7faaac2e5f4719ab116
faysalzaman <faysalzaman00@gmail.com>
2024-07-04 11:32:42 +0500

    chore: Update app URLs to use new Azure deployment

--- lib/constant/app_urls.dart
+++ lib/constant/app_urls.dart
@@ -1,6 +1,8 @@
 abstract class AppUrls {
   // static const String baseUrl = "http://173.249.56.16:5050";
-  static const String baseUrl = "http://api.becappksa.com";
   // static const String host = "173.249.56.16:5050";
-  static const String host = "api.becappksa.com";
+  // static const String baseUrl = "http://api.becappksa.com";
+  // static const String host = "api.becappksa.com";
+  static const String baseUrl = "https://becbarcodeapp.azurewebsites.net";
+  static const String host = "becbarcodeapp.azurewebsites.net";
 }
