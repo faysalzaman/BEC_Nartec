d6fe2ea85c3948510f0850a2388eaa504eb7fd30
Faysal Zaman <123411654+faysalzaman@users.noreply.github.com>
2025-02-15 20:44:03 +0500

    chore: Switch base URL between development and production environments

--- lib/constant/app_urls.dart
+++ lib/constant/app_urls.dart
@@ -5,7 +5,7 @@ abstract class AppUrls {
 
   // static const String baseUrl = "https://becbarcodeapp.azurewebsites.net";
 
-  static const String baseUrl = "http://gs1Ksa.org:5050";
+  // static const String baseUrl = "http://gs1Ksa.org:5050";
 
-  // static const String baseUrl = "https://portal.becarabia.com:5999";
+  static const String baseUrl = "https://portal.becarabia.com:5999";
 }
